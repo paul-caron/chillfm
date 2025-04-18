20c8928030a0753610da168081e69e31582093b1
paul <paul@webler.com>
2025-04-18 01:31:18 -0500

    update

--- src/patchmemory.js
+++ src/patchmemory.js
@@ -1,9 +1,10 @@
+let presetPatches = [{"name":"brassySound","settings":{"range":["0","0.44","0.29","0","0.29","0","0.1","0.1","0.1","0.1","0.4","0.4","0.4","0.4","0.7","0.7","0.7","0.7","2","2","2","2","341","0","0","0","0","324","0","0.01","0"],"select":["0","sine","sine","sine","sine"],"checkbox":[false,false,false,false,false],"number":["1","1","1","1","0","0","0","0"]}},{"name":"meow-animal","settings":{"range":["0","1","0.38","0","0","0","0.1","0.1","0.1","0.1","0.4","0.4","0.4","0.4","0.7","0.7","0.7","0.7","0.85","1.8","2","0.67","0","0","0","446","0","0","621.42","0.01","0"],"select":["2","sine","sine","sine","sine"],"checkbox":[false,false,false,false,false],"number":["1","1","1","1","0","0","0","0"]}},{"name":"organSound","settings":{"range":["0","0.19","0.7","0.53","0.32","0.17","0.01","0.08","0.01","0.03","0.1","0.15","0.4","0.4","0.93","0.88","0.74","0.7","0.46","0.69","0.57","0.73","0","0","0","0","0","0","0","0.01","0"],"select":["0","sine","sine","sine","sine"],"checkbox":[false,false,false,false,false],"number":["1","2","3","5","4","-3","3","-1"]}},{"name":"organSound2","settings":{"range":["0","0.19","0.7","0.53","0.32","0.05","0.07","0.08","0.01","0.03","0.1","0.15","0.4","0.4","1","0.93","0.85","0.7","0.46","0.69","0.57","0.73","0","0","0","0","0","0","0","7.39","179.03"],"select":["0","sine","sine","sine","sine"],"checkbox":[false,false,false,false,true],"number":["1","2","3","5","4","-3","3","-1"]}}];
+
+
 function deletePatch(){
     let select = document.querySelector("#patch-memory select");
     let name = select.options[select.selectedIndex].text
     if(!name) return;
-
-
 
     let patches = JSON.parse(localStorage.getItem('patches'));
     if(!patches)
@@ -70,7 +71,7 @@ function loadPatch(){
 function updatePatchesList(){
     let select = document.querySelector("#patch-memory select");
     select.innerHTML = "";
-    let patches = JSON.parse(localStorage.getItem('patches'));
+    let patches = [...presetPatches, ...JSON.parse(localStorage.getItem('patches'))];
     if(patches){
         patches.forEach(patch => {
             let option = document.createElement("option");
